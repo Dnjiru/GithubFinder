747909fd061e5be88bb5d7a51232db71eb0d5e34
SophiaNM <njerimurage92@outlook.com>
2018-05-31 13:51:56 +0300

    added styling to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {RoutingModule} from './routing/routing.module'
 
+
+
+// Defining routes
+// const routes:Routes=[
+//   {path:"users",component:UsersComponent},
+//   {path:"repos",component:ReposComponent},
+//   {path:"",redirectTo:"/users",pathMatch:"full"},
+//   {path:'**',component:NotFoundComponent}
+// ]
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ReposComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
