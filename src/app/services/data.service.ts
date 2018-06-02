84af8dae7f8e228fd436946ece6a161d28ce6b92
SophiaNM <njerimurage92@outlook.com>
2018-06-02 23:15:47 +0300

    adding observables to the services

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+// import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {environment} from '../../environments/environment'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  
+  private username: string;
+ 
+
+  constructor(private http:Http) {
+    console.log('service is ready');
+    this.username = 'SophiaNM';
+   }
+
+   getProfile(){
+     return this.http.get(environment.apiUrl+ this.username + "?access_token=" + environment.accessToken).pipe(map(res => res.json()));
+   }
 }
+  
